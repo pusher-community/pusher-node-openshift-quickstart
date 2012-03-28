b9f7b8ded01b43b887262528c4f70c277ba9c826
U-monsterBox\monster <monster@monsterBox.(none)>
2012-03-28 14:00:57 -0400

    test

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ http.createServer(function (req, res) {
 	}
 	
   res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end("Welcome to Node.js on OpenShift!\n\n");
   res.end("Your IP address seems to be " + addr + "\n");
 }).listen(port, ipaddr);
 console.log("Server running at http://" + ipaddr + ":" + port + "/");
