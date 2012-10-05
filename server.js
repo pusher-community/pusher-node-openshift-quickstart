68e54e71e76dac92aa53e46e912cc8c03fa02c12
Ram Ranganathan <ramr@redhat.com>
2012-10-04 17:31:37 -0700

    Fix variables for dev/typeless.

--- server.js
+++ server.js
@@ -3,8 +3,8 @@
 var http = require('http');
 
 //Get the environment variables we need.
-var ipaddr  = process.env.OPENSHIFT_INTERNAL_IP || "127.0.0.1";
-var port    = process.env.OPENSHIFT_INTERNAL_PORT || 8080;
+var ipaddr  = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port    = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 http.createServer(function (req, res) {
 	var addr = "unknown";
