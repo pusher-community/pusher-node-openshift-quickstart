677ab78a1f1c8bd026d775bc79d8f5a5d1c4185b
Phil Leggetter <phil@leggetter.co.uk>
2013-06-04 09:40:36 +0100

    adding dependencies - using expressjs

--- server.js
+++ server.js
@@ -1,30 +1,14 @@
 #!/bin/env node
-//  OpenShift sample Node application
-var http = require('http');
+
+var express = require('express');
+var app = express();
 
 //Get the environment variables we need.
 var ipaddr  = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port    = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-http.createServer(function (req, res) {
-	var addr = "unknown";
-	var out = "";
-	if (req.headers.hasOwnProperty('x-forwarded-for')) {
-		addr = req.headers['x-forwarded-for'];
-	} else if (req.headers.hasOwnProperty('remote-addr')){
-		addr = req.headers['remote-addr'];
-	}
+app.use(express.static(__dirname + '/public'));
 
-	if (req.headers.hasOwnProperty('accept')) {
-		if (req.headers['accept'].toLowerCase() == "application/json") {
-			  res.writeHead(200, {'Content-Type': 'application/json'});
-			  res.end(JSON.stringify({'ip': addr}, null, 4) + "\n");			
-			  return ;
-		}
-	}
-	
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write("Welcome to Node.js on OpenShift!\n\n");
-  res.end("Your IP address seems to be " + addr + "\n");
-}).listen(port, ipaddr);
-console.log("Server running at http://" + ipaddr + ":" + port + "/");
+app.listen( port, ipaddr, function() {
+ console.log( 'listening on port ' + port );
+} );
