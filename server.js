760c3b087363e97015d1955f5e3c42a6f2b863ab
Phil Leggetter <phil@leggetter.co.uk>
2013-06-04 10:30:24 +0100

    adding Pusher

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 #!/bin/env node
 
 var express = require('express');
+var Pusher = require( 'pusher' )
+
 var app = express();
+var config = require( __dirname + '/config.json' );
 
 //Get the environment variables we need.
 var ipaddr  = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port    = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));
+
+app.get( '/trigger', function( req, res ) {
+  var pusher = new Pusher( config.pusher );
+  var data =  {
+                message: 'Hello World using Pusher on OpenShift!'
+              };
+  pusher.trigger( 'test-channel', 'test-event', data, null, function( err, pusherReq, pusherRes ) {
+      res.json( pusherRes.statusCode, { status: pusherRes.statusCode } );
+  } );
+  
+} );
 
 app.listen( port, ipaddr, function() {
  console.log( 'listening on port ' + port );
