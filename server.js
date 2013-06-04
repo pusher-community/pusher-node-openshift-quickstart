57aadf937410595c7dc862424abd61ac6817d297
Phil Leggetter <phil@leggetter.co.uk>
2013-06-04 10:43:33 +0100

    updating to use EJS template

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ var ipaddr  = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port    = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));
+app.set( 'views', __dirname + '/views' );
+app.set( 'view engine', 'ejs' );
+
+app.get( '/', function( req, res ) {
+  res.render('index', { appKey: config.pusher.key } );
+});
 
 app.get( '/trigger', function( req, res ) {
   var pusher = new Pusher( config.pusher );
@@ -20,7 +26,7 @@ app.get( '/trigger', function( req, res ) {
   pusher.trigger( 'test-channel', 'test-event', data, null, function( err, pusherReq, pusherRes ) {
       res.json( pusherRes.statusCode, { status: pusherRes.statusCode } );
   } );
-  
+
 } );
 
 app.listen( port, ipaddr, function() {
